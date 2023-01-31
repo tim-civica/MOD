00c5536cd0ef4e574c021a72e08ab8e3d3baccb1
Tim Samanchi - CIVICA <tim.samanchi@civica.co.uk>
2023-01-31 14:44:21 +0000

    testing git

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,18 @@ terraform {
   required_providers {
     aws = {
      source  = "hashicorp/aws"
-     version = "~> 2.70.0"
+     version = "~> 4.0"
     }
   }
   required_version = ">= 0.13"
 }
+provider "aws" {
+  region = "eu-west-2"
+}
 resource "aws_vpc" "test" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    "Name" = "CIVICA-test"
+  }
 }
